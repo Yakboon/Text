339727cc5d42b60daf6add20f5afd40f0c0c9fb8
Yakboon <120273912+Yakboon@users.noreply.github.com>
2022-12-17 20:21:15 +0300

    Text space
    how to add space between letters in a text....

---
+++ Textspace/src/Study.java
@@ -0,0 +1,13 @@
+
+public class Study {
+
+	public static void main(String[] args) {
+		String str = " Your destiny will come to you just be patient";
+
+		for (int i = 0; i <= str.length() - 1; i++) {
+			System.out.print(str.charAt(i) + " ");
+		}
+		System.out.println(":)");
+	}
+
+}
